e25d2a4ddbf87ab0225f53d8260af81b559a997a
Cédric BRASSEUR <50497186+cbrasseur@users.noreply.github.com>
2024-02-27 16:22:18 +0100

    Update homeView.php

--- web/views/homeView.php
+++ web/views/homeView.php
@@ -3,11 +3,11 @@
     <div style="width:100%;height:0;padding-bottom:68%;position:relative;"><iframe src="https://giphy.com/embed/3oFzmdjqH15YebLQ52" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
     <form action="game" method="post">
         <div class="input-wrapper text">
-            <label for="p1">Pseudo joueur 1 :</label>
+            <label for="p1">Pseudo premier joueur :</label>
             <input type="text" name="p1" id="p1" required maxlength="10" minlength="3">
         </div>
         <div class="input-wrapper text">
-            <label for="p2">Pseudo joueur 2 :</label>
+            <label for="p2">Pseudo second joueur :</label>
             <input type="text" name="p2" id="p2" required maxlength="10" minlength="3">
         </div>
         <div class="input-wrapper submit">
